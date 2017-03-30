56fa60ad7a1b2ea8c514f93dc76e342dd72a37ed
Brendon Barreto <brendondominic@gmail.com>
2017-03-30 12:39:22 -0300

    C# Pilebox
    Introduction to the project

---
+++ CSharp/PileBox/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PileBox
+{
+	public class Box
+	{
+		public Box(int size)
+		{
+			Width = size;
+			Height = size;
+		}
+
+		public Box(int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
+
+		public int Height { get; set; }
+
+		public int Width { get; set; }
+
+		public Pile Pile { get; set; }
+	}
+}
---
+++ CSharp/PileBox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PileBox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Somente caixas quadradas
+			Pile p1 = new Pile();
+			p1.AddBox(new Box(1));
+			p1.AddBox(new Box(2));
+			p1.AddBox(new Box(1));
+			p1.AddBox(new Box(1));
+			p1.AddBox(new Box(5));
+			p1.AddBox(new Box(3));
+			p1.AddBox(new Box(2));
+			p1.AddBox(new Box(4));
+		}
+	}
+}
