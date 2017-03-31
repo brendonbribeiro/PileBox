fc6dd17bbbdba2cd76c5f320678761bfb72035b0
Brendon Barreto <brendondominic@gmail.com>
2017-03-30 22:02:06 -0300

    C# Testcase

--- solutions/csharp/PileBox/Program.cs
+++ solutions/csharp/PileBox/Program.cs
@@ -10,16 +10,37 @@ namespace PileBox
 	{
 		static void Main(string[] args)
 		{
-			//Somente caixas quadradas
 			Pile p1 = new Pile();
-			p1.AddBox(new Box(1));
-			p1.AddBox(new Box(2));
-			p1.AddBox(new Box(1));
-			p1.AddBox(new Box(1));
-			p1.AddBox(new Box(5));
-			p1.AddBox(new Box(3));
-			p1.AddBox(new Box(2));
-			p1.AddBox(new Box(4));
+			p1.AddBox(new Box(2, 2));
+			p1.AddBox(new Box(1, 1));
+			p1.AddBox(new Box(1, 2));
+			p1.AddBox(new Box(2, 1));
+			var h1 = p1.Height;
+
+			Pile p2 = new Pile();
+			p2.AddBox(new Box(5, 7));
+			p2.AddBox(new Box(3, 4));
+			p2.AddBox(new Box(1, 2));
+			p2.AddBox(new Box(2, 3));
+			p2.AddBox(new Box(4, 2));
+			p2.AddBox(new Box(4, 4));
+			var h2 = p2.Height;
+
+			Pile p3 = new Pile();
+			p3.AddBox(new Box(3, 1));
+			p3.AddBox(new Box(2, 2));
+			p3.AddBox(new Box(1, 3));
+			p3.AddBox(new Box(6, 9));
+			p3.AddBox(new Box(5, 8));
+			p3.AddBox(new Box(3, 7));
+			p3.AddBox(new Box(4, 2));
+			p3.AddBox(new Box(3, 1));
+			p3.AddBox(new Box(2, 2));
+			p3.AddBox(new Box(1, 4));
+			var h3 = p3.Height;
+
+			Console.WriteLine(h1 == 4 && h2 == 11 && h3 == 14);
+			Console.ReadKey();
 		}
 	}
 }
